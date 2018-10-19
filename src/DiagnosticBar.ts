3bbb551bf142ce89c32cf073ec79cb8eb3e7fa2c
Joe Berria <joeberria@gmail.com>
2018-10-19 12:59:59 -0700

    using a map to handle the editors diagnostic messages

--- src/DiagnosticBar.ts
+++ src/DiagnosticBar.ts
@@ -7,44 +7,58 @@ import {
   TextEditor,
   TextEditorSelectionChangeEvent,
   DiagnosticChangeEvent,
-  Diagnostic,
 } from 'vscode';
 
 
-enum Severity {
-  INFO,
-  WARNING,
-  ERROR,
+interface IDiagnosticMessage {
+  message: string;
+  severity: number;
+  line: number;
 }
 
 export class DiagnosticBar implements Disposable {
   private _statusBarItem: StatusBarItem;
   private _disposables: Disposable[];
-  private _currentDiagnostics: Diagnostic[];
   private _currentDocURI: Uri;
+  private _currentDiagnostics: Map<string, IDiagnosticMessage[]>;
 
   constructor(item: StatusBarItem) {
     this._disposables = [];
-    this._currentDiagnostics = [];
+    this._currentDiagnostics = new Map();
     this._statusBarItem = item;
     this._currentDocURI = window.activeTextEditor ? window.activeTextEditor.document.uri : Uri.file('.');
 
     this._disposables.push(Disposable.from(this._statusBarItem));
-    this._disposables.push(languages.onDidChangeDiagnostics(this.diagnosticChangedListener));
+    this._disposables.push(languages.onDidChangeDiagnostics((e) => this.diagnosticChangedListener(e)));
   }
 
   public activeEditorChanged(editor: TextEditor): void {
+    const issues = languages.getDiagnostics(this._currentDocURI);
+    const dMessage: IDiagnosticMessage[] = issues.map((e) => {
+      return {
+        line: e.range.start.line,
+        severity: e.severity,
+        message: `[${e.source}] ${e.message}`,
+      };
+    });
+
     this._currentDocURI = editor.document.uri;
-    this._currentDiagnostics = languages.getDiagnostics(this._currentDocURI);
+    this._currentDiagnostics.set(editor.document.uri.path, dMessage);
   }
 
   public cursorSelectionChangedListener(selection: TextEditorSelectionChangeEvent): void {
-    for (const diagnostic of this._currentDiagnostics) {
-      if (diagnostic.range.contains(selection.selections[0].active)) {
-        // todo setup message
-        this._statusBarItem.text = diagnostic.message;
-        this._statusBarItem.show();
-      }
+    const messages = this._currentDiagnostics.get(this._currentDocURI.path);
+    const cursorLine = selection.selections[ 0 ].active.line;
+
+    if (!messages || messages.length === 0) { return; }
+
+    const lintMessage = messages.find((elem) => elem.line === cursorLine);
+    if (!!lintMessage) {
+      this._statusBarItem.text = lintMessage.message;
+      this._statusBarItem.show();
+
+    } else {
+      this._statusBarItem.hide();
     }
   }
 
@@ -56,8 +70,17 @@ export class DiagnosticBar implements Disposable {
 
   private diagnosticChangedListener(diagnostic: DiagnosticChangeEvent): void {
     for (const uri of diagnostic.uris) {
-      if (!!this._currentDocURI.path.match(uri.path)) {
-        this._currentDiagnostics = languages.getDiagnostics(uri);
+      if (this._currentDiagnostics.has(uri.path)) {
+        const issues = languages.getDiagnostics(uri);
+        const dMessage: IDiagnosticMessage[] = issues.map((e) => {
+          return {
+            line: e.range.start.line,
+            severity: e.severity,
+            message: `[${e.source}] ${e.message}`,
+          };
+        });
+
+        this._currentDiagnostics.set(uri.path, dMessage);
       }
     }
   }
